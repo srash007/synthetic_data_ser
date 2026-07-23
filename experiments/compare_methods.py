6869c6a6e38549227f538dc3f40404cfab29476a
srash007 <sarahrashiwa@gmail.com>
2026-07-22 20:03:57 -0400

    v

--- experiments/compare_methods.py
+++ experiments/compare_methods.py
@@ -6,8 +6,6 @@ import pandas as pd
 
 from sklearn.linear_model import (
     LinearRegression,
-    Ridge,
-    Lasso,
 )
 
 
@@ -22,45 +20,33 @@ from .run_experiment import run_experiment
 
 
 generator = FunctionGenerator(
-    function="linear",
-    n_samples=1000,
-    n_features=1,
+    function="cubic",
+    n_samples=10000,
+    n_features=20,
     random_state=42
 )
 
 X,y = generator.generate()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+thresholds = [0.85, 0.90, 0.95,0.99]
 
 results = []
 
-results.append(run_experiment(
-    X_train,
-    X_test,
-    y_train,
-    y_test,
-    model=SERRegressor("B"),
-))
+for threshold in thresholds:
 
-results.append(run_experiment(
-    X_train,
-    X_test,
-    y_train,
-    y_test,
-    model=LinearRegression(),
-))
-
-results.append(run_experiment(
-    X_train,
-    X_test,
-    y_train,
-    y_test,
-    model=LinearRegression(),
-    preprocessing=SMOGNPreprocessor(
-        rel_thres=0.80,
-    ),
-))
+    results.append(
+        run_experiment(
+            X_train,
+            X_test,
+            y_train,
+            y_test,
+            model=LinearRegression(),
+            preprocessing=SMOGNPreprocessor(
+                rel_thres=threshold
+            ),
+        )
+    )
 
 df = pd.DataFrame(results)
 
