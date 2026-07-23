102b0a4f9a6defe0cd5ea3cd8f6578c1623b4783
srash007 <sarahrashiwa@gmail.com>
2026-07-22 20:15:30 -0400

    v

--- experiments/compare_methods.py
+++ experiments/compare_methods.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.linear_model import (
     LinearRegression,
+    Ridge,
+    Lasso,
 )
 
 
@@ -22,32 +24,48 @@ from .run_experiment import run_experiment
 generator = FunctionGenerator(
     function="cubic",
     n_samples=10000,
-    n_features=20,
+    n_features=1,
     random_state=42
 )
 
 X,y = generator.generate()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-thresholds = [0.85, 0.90, 0.95,0.99]
+
 
 results = []
 
-for threshold in thresholds:
+results.append(run_experiment(
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    model=SERRegressor("B"),
+))
 
-    results.append(
-        run_experiment(
-            X_train,
-            X_test,
-            y_train,
-            y_test,
-            model=LinearRegression(),
-            preprocessing=SMOGNPreprocessor(
-                rel_thres=threshold
-            ),
-        )
-    )
+results.append(run_experiment(
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    model=LinearRegression(),
+))
+
+results.append(run_experiment(
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    model=LinearRegression(),
+    preprocessing=SMOGNPreprocessor(
+        rel_thres=0.99,
+    ),
+))
 
 df = pd.DataFrame(results)
 
 print(df)
+df.to_csv(
+    "results/methodCubic.csv",
+    index=False,
+)
